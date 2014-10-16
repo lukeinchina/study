f8787569ade173c32041465c8edc457ea6ac525c
lukeinchina <lukeinchina@gmail.com>
2014-10-16 16:43:20 +0800

    init gitignore

---
+++ algorithm/divide_and_conquer/perm.c
@@ -0,0 +1,20 @@
+/*
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+#include <stdio.h>
+
+typedef data_type int;
+#define swap(a, b) do {\
+    date_type temp_elem = a; a = b; b = temp_elem;\
+    while(0);
+int
+main(void)
+{
+    return 0;
+}
